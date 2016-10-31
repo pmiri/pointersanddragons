bd266b53fd566d0d80df8d0033906f6056644d82
dsvoid <dsvoid@gmail.com>
2016-10-31 14:59:45 -0400

    Add files via upload

---
+++ Character/Fighter.cpp
@@ -0,0 +1,31 @@
+#include "Character.h"
+
+class Fighter : public Character
+{
+public:
+	//! The default Character constructor is called, followed bythe Fighter constructor to set up character stats
+	Fighter()
+	{
+		className = "Fighter";
+		//fighter max hit points is initially 10
+		maxHitPoints = 10;
+		armorClass = 10;
+		initAbilityModifiers();
+	}
+
+	Fighter(int str, int dex, int con, int intel, int wis, int cha)
+	{
+		className = "Fighter";
+		//fighter max hit points is initially 10, then affected by ability modifiers
+		maxHitPoints = 10;
+		initAbilityModifiers();
+	}
+
+	int levelUp(int diceRoll)
+	{
+		level++;
+		maxHitPoints = maxHitPoints + diceRoll;
+		int skillPoints = 2 + abilityScores[3];
+		distributePoints(skillPoints);
+	}
+};
