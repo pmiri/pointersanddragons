777e5ce1bf2791bceaff328bf2cd24d3d33df797
jabob <jacob.desrochers1@gmail.com>
2016-11-07 15:19:33 -0500

    Map Builder Added
    Merging map builder from branch

---
+++ comp345game/MapBuilder.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include "Map.h"
+
+using namespace std;
+
+class MapBuilder
+{
+public:
+	static Map *buildFromFile(string path);
+	static Map *buildFromMap(Map map);
+
+private:
+
+
+};
