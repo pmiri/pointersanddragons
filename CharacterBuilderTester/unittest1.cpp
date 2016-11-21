9fe626e8944a8e562b02a83f42c6c3435dcd02c6
Disva <dravirdisva@gmail.com>
2016-11-21 15:18:33 -0500

    Added testing project

---
+++ CharacterBuilderTester/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "CharacterBuilder.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CharacterBuilderTester
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(BullyBuilder)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
