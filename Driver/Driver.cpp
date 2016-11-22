e463856bee7a35425b17c3622f5c651512557343
Disva <dravirdisva@gmail.com>
2016-11-22 14:14:46 -0500

    Added driver

--- Driver/Driver.cpp
+++ Driver/Driver.cpp
@@ -4,16 +4,24 @@
 #include "stdafx.h"
 #include "BullyBuilder.h"
 #include "CharacterCreator.h"
-
+#include <ctime>
 int main()
 {
 	BullyBuilder* bulBuild = new BullyBuilder();
+	string pause;
 	CharacterCreator charCreator;
 	charCreator.setCharacterBuilder(bulBuild);
+	srand(time(NULL));
 	charCreator.createCharacter(1);
 	Character testChar = *charCreator.getCharacter();
-	bool goodstats = false;
+	std::cout << "Made a level 1 bully\n";
 	int* stats = testChar.getAbilityScores();
-	int test = *(stats + 1);
+	std::cout << "Strength: " << *(stats + 0) << "\n";
+	std::cout << "Dexterity: " << *(stats + 1) << "\n";
+	std::cout << "Constitution: " << *(stats + 2) << "\n";
+	std::cout << "Intelligence: " << *(stats + 3) << "\n";
+	std::cout << "Wisdom: " << *(stats + 4) << "\n";
+	std::cout << "Charisma: " << *(stats + 5) << "\n";
+	std::cin >> pause;
 }
 
